bf636543f809a1237740a4720466ae83a1ffd145
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-05-14 23:14:50 +0600

    use cors :tada:

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-// Middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
